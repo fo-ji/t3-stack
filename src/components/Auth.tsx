d8730b3df02a9f00f74b2210263e93835364f042
fo-ji <fumiya1753@gmail.com>
2023-01-09 15:06:59 +0900

    implement auth

---
+++ src/components/Auth.tsx
@@ -0,0 +1,12 @@
+import { signIn } from "next-auth/react";
+
+export const Auth = () => (
+  <div>
+    <button
+      className="rounded bg-blue-600 py-2 px-4 font-bold text-white hover:bg-blue-800"
+      onClick={() => signIn("github")}
+    >
+      GitHub Auth
+    </button>
+  </div>
+);
